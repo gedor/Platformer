6f5489d0a04eab5b8adef273d4325050f689fd05
DESKTOP-CU92D4B\lovro <gedgedor@gmail.com>
2018-07-14 14:12:25 +0200

    Rijesio problem sa sporim micanjem Cursora, sada se brzina moze kontrolirati.

--- Assets/Scripts/CursorMoveController.cs
+++ Assets/Scripts/CursorMoveController.cs
@@ -10,7 +10,7 @@ public class CursorMoveController : MonoBehaviour {
 		private float horizontal;
 		private float vertical;
 		
-	//	public float cursSpeed;
+		public float cursSpeed;
 
 	// Use this for initialization
 	void Awake () {
@@ -25,7 +25,7 @@ public class CursorMoveController : MonoBehaviour {
 		horizontal = Input.GetAxis("Horizontal");
 		vertical = Input.GetAxis("Vertical");
 		Vector2 currCursPos = CursorControl.GetGlobalCursorPos();
-		Vector2 cursPos = new Vector2(currCursPos.x + horizontal,currCursPos.y + vertical * -1.0f);
+		Vector2 cursPos = new Vector2(currCursPos.x + horizontal * cursSpeed,currCursPos.y + vertical * cursSpeed  * -1.0f);
 		if(tel.teleTrigger == true){
 
 			CursorControl.SetGlobalCursorPos(cursPos);
