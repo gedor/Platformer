04a5ff0a5da3ce3c67e3bbec6c7f41344ed8f812
DESKTOP-CU92D4B\lovro <gedgedor@gmail.com>
2018-07-21 00:41:13 +0200

    Postavio sam kod za mijenjanje Cursora a referenca za Cursor Image je na Pause Menu i Teleporter. Nesto ne radi.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -10,6 +10,14 @@ public class PauseMenu : MonoBehaviour {
 	public Canvas pauseCanvas;
 	public Teleporter  tele;
 	public bool paused = false;
+
+
+	public CursorMode cursorMode = CursorMode.Auto;
+	public Vector2 hotspotZero = Vector2.zero;
+	public Texture2D cursorTexture;
+
+	public Texture2D cursorTele;
+	public Vector2 hotspot;
 	// Use this for initialization
 	void Awake() {
 		pauseCanvas.enabled = false;
@@ -24,38 +32,44 @@ public class PauseMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+			hotspot = new Vector2 (cursorTele.width/2,cursorTele.height/2);
+
 		if(Input.GetButtonDown("Cancel")){
-		if(pauseCanvas.enabled == false){
+				if(pauseCanvas.enabled == false){
 
 			pauseCanvas.enabled = true;
+			Cursor.SetCursor(cursorTexture,hotspotZero,cursorMode);
 			Cursor.lockState = CursorLockMode.None; 
 			Cursor.visible = true;
 			paused = true;
 			Time.timeScale = 0.0f;
 			
 
-		}else if (pauseCanvas.enabled == true){
+		}
+			else if (pauseCanvas.enabled == true){
 			
 
 
-			if(tele.teleTrigger == false){
+				if(tele.teleTrigger == false){
+
 				Time.timeScale = 1.0f;
+				Cursor.SetCursor(cursorTexture,hotspotZero,cursorMode);
 			Cursor.visible = false;
 			Cursor.lockState = wantedMode;
 			pauseCanvas.enabled = false;
 			paused = false;
 			
-			}
-			else if(tele.teleTrigger == true){
+				}
+				else if(tele.teleTrigger == true){
 				Time.timeScale = 1.0f;
 				Cursor.lockState = CursorLockMode.None; 
 				Cursor.visible = true;
 				paused = false;
 				pauseCanvas.enabled = false;
-				
+				Cursor.SetCursor(cursorTele,hotspot,cursorMode);
 
+				}
 			}
-		}
 		}
 	}
 	public void ResumeGame(){
