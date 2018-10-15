aa3c56b820eae5e25d3b5e96ae463afb27484405
DESKTOP-CU92D4B\lovro <gedgedor@gmail.com>
2018-10-15 08:25:04 +0200

    Update na Unity 2018.2.10f1

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+	public LayerMask enemyMask;
+	private Rigidbody2D enemyRigid;
+	Transform enemyTran;
+	float myWidth;
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+		enemyTran = this.transform;
+		enemyRigid = this.GetComponent<Rigidbody2D>();
+		myWidth = this.GetComponent<SpriteRenderer>().bounds.extents.x;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	Vector2 lineCastPos = enemyTran.position - enemyTran.right * myWidth;
+	bool IsGrounded = Physics2D.Linecast(lineCastPos, lineCastPos + Vector2.down, enemyMask);
+	if(!IsGrounded){
+		Vector3 currRot = enemyTran.eulerAngles;
+		currRot.y += 180;
+		enemyTran.eulerAngles = currRot;
+	}
+	MoveEnemy();
+	}
+	void MoveEnemy(){
+		Vector2 myVel =enemyRigid.velocity;
+		myVel.x = enemyTran.right.x * speed * Time.deltaTime;
+		enemyRigid.velocity = myVel;
+	}
+}
